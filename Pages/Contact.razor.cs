559dec97fed3a52b26fd36b6eb51118e5f3e6c43
HMZ <hamzi1995@gmail.com>
2022-01-10 01:05:07 +0200

    Add contact page link + page

---
+++ Pages/Contact.razor.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HMZ4th.Pages
+{
+    public partial class Contact : ComponentBase, IAsyncDisposable
+    {
+        [Inject] IJSRuntime JSRuntime { get; set; }
+        IJSObjectReference BlogModule;
+
+        bool InitAnimationPlayed;
+
+        protected async override Task OnInitializedAsync()
+        {
+            BlogModule = await JSRuntime.InvokeAsync<IJSObjectReference>("import", "./js/modules/blogModule.js");
+        }
+        protected async override Task OnAfterRenderAsync(bool firstRender)
+        {
+            if (!firstRender)
+            {
+                if (InitAnimationPlayed == false)
+                {
+                    await BlogModule.InvokeVoidAsync("InitAnimation");
+                    InitAnimationPlayed = true;
+                }
+            }
+        }
+
+        public async ValueTask DisposeAsync()
+        {
+            if (BlogModule != null)
+            {
+                await BlogModule.InvokeVoidAsync("Dispose");
+                await BlogModule.DisposeAsync();
+            }
+        }
+    }
+}
