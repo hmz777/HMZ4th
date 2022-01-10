7748d36be940f6d747c7ddfd56c86d7be79ab236
HMZ <hamzi1995@gmail.com>
2022-01-11 00:12:41 +0200

    Add stagger animation to the dashboard page

--- wwwroot/js/modules/dashboardModule.js
+++ wwwroot/js/modules/dashboardModule.js
@@ -13,6 +13,21 @@ export function InitAnimation() {
         autoPlay: true,
         loop: false
     });
+
+    Stagger();
+}
+
+export function Stagger() {
+    anime({
+        targets: "#dashboard-block .dashboard-block",
+        translateY: [50, 0],
+        opacity: [0, 1],
+        easing: "easeOutQuint",
+        duration: 1000,
+        delay: anime.stagger(100, { start: 100 }),
+        autoPlay: true,
+        loop: false
+    });
 }
 
 export function Dispose() {
