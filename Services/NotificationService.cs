feaa9fad1cb314c18d823c8db0610559bee21fe9
HMZ <hamzi1995@gmail.com>
2022-01-14 00:36:01 +0200

    Create NotificationService.cs

---
+++ Services/NotificationService.cs
@@ -0,0 +1,19 @@
+﻿using HMZ4th.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HMZ4th.Services
+{
+    public class NotificationService
+    {
+        public event Action<NotificationMessageModel> OnMessageReceive;
+
+        public void Show(NotificationMessageModel notificationMessageModel)
+        {
+            OnMessageReceive?.Invoke(notificationMessageModel);
+        }
+    }
+}
