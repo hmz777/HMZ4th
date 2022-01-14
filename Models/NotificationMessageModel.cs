6d3f8e241aab40f59b2c78dd465e442caae8c459
HMZ <hamzi1995@gmail.com>
2022-01-14 23:09:38 +0200

    Implement a notifications container from scratch and rewire stuff to use it.

--- Models/NotificationMessageModel.cs
+++ Models/NotificationMessageModel.cs
@@ -8,6 +8,7 @@ namespace HMZ4th.Models
 {
     public class NotificationMessageModel
     {
+        public string NotificationId { get; set; }
         public NotificationType NotificationType { get; set; }
         public string NotificationMessage { get; set; }
     }
--- Services/HttpClientDelegator.cs
+++ Services/HttpClientDelegator.cs
@@ -9,32 +9,54 @@ namespace HMZ4th.Services
     public class HttpClientDelegator : DelegatingHandler
     {
         private readonly TransitionPageService transitionPageService;
+        private readonly NotificationService notificationService;
 
-        public HttpClientDelegator(TransitionPageService transitionPageService)
+        public HttpClientDelegator(TransitionPageService transitionPageService, NotificationService notificationService)
         {
             this.transitionPageService = transitionPageService;
+            this.notificationService = notificationService;
         }
         protected override async Task<HttpResponseMessage> SendAsync(
                                        HttpRequestMessage request,
                                        CancellationToken cancellationToken = default)
         {
 
-            if (cancellationToken.IsCancellationRequested)
+            try
             {
-                //Cancel
+                if (cancellationToken.IsCancellationRequested)
+                {
+                    //Cancel
+                    transitionPageService.DoTransition(false);
+
+                    notificationService.Show(new Models.NotificationMessageModel
+                    {
+                        NotificationMessage = "Operation canceled!",
+                        NotificationType = Models.NotificationType.Warning
+                    });
+
+                    return default;
+                }
+                else
+                    //Enable the loader component
+                    transitionPageService.DoTransition(true);
+
+                var ResponseMessage = await base.SendAsync(request, cancellationToken);
+
+                //Disable the loader component
                 transitionPageService.DoTransition(false);
+
+                return ResponseMessage;
+            }
+            catch
+            {
+                notificationService.Show(new Models.NotificationMessageModel
+                {
+                    NotificationMessage = "Operation failed!",
+                    NotificationType = Models.NotificationType.Error
+                });
+
                 return default;
             }
-            else
-                //Enable the loader component
-                transitionPageService.DoTransition(true);
-
-            var ResponseMessage = await base.SendAsync(request, cancellationToken);
-
-            //Disable the loader component
-            transitionPageService.DoTransition(false);
-
-            return ResponseMessage;
         }
     }
 }
