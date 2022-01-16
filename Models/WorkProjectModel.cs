3fec1e0500df3d0766ea4ea9d8ba63f027643d83
HMZ <hamzi1995@gmail.com>
2022-01-16 13:07:51 +0200

    Update the work section

---
+++ Models/WorkProjectModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HMZ4th.Models
+{
+    public class WorkProjectModel
+    {
+        public string name { get; set; }
+        public string description { get; set; }
+        public object[] topics { get; set; }
+        public int stargazers_count { get; set; }
+        public int watchers_count { get; set; }
+        public int forks_count { get; set; }
+        public bool IsStaticProject { get; set; }
+    }
+}
