72acc5ce5fbc7b52ae19b6665fe56a05c26e6f3f
HMZ <hamzi1995@gmail.com>
2022-01-18 01:21:12 +0200

    Update the blog section

--- wwwroot/js/modules/blogModule.js
+++ wwwroot/js/modules/blogModule.js
@@ -13,6 +13,17 @@ export function InitAnimation() {
         autoPlay: true,
         loop: false
     });
+
+    anime({
+        targets: '.content-article',
+        opacity: [0, 1],
+        translateY: [50, 0],
+        easing: "easeOutQuint",
+        duration: GlobalOptions.HeaderAnimationDuration,
+        delay: anime.stagger(100, { start: 100 }),
+        autoPlay: true,
+        loop: false
+    });
 }
 
 export function Dispose() {
