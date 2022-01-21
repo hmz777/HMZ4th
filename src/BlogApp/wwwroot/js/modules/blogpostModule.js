c916aee84f2c748d71c5b9b5ec4e39d6d5ff5fb0
HMZ <hamzi1995@gmail.com>
2022-01-21 11:45:50 +0200

    Update blogpostModule.js

--- src/BlogApp/wwwroot/js/modules/blogpostModule.js
+++ src/BlogApp/wwwroot/js/modules/blogpostModule.js
@@ -5,7 +5,16 @@
 //#region API
 
 export function InitAnimation() {
-
+    anime({
+        targets: '.post-desc,.post-footer',
+        opacity: [0, 1],
+        translateY: [50, 0],
+        easing: "easeOutQuint",
+        duration: GlobalOptions.HeaderAnimationDuration,
+        delay: anime.stagger(200, { start: 100 }),
+        autoPlay: true,
+        loop: false
+    });
 }
 
 export function Dispose() {
