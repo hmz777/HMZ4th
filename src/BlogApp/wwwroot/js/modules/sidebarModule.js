ed4aa65bf276e2658852d02373af4eb362e763e4
HMZ <hamzi1995@gmail.com>
2022-01-24 00:11:41 +0200

    Add logic for closing the sidebar when clicking links on mobile view

---
+++ src/BlogApp/wwwroot/js/modules/sidebarModule.js
@@ -0,0 +1,12 @@
+﻿var sidebar = document.querySelector(".sidebar");
+var sidebarBtn = document.querySelector(".sidebar-toggle");
+
+export function InitHandler(componentRef) {
+    if (window.matchMedia("(max-width: 991px)").matches) {
+        document.onclick = function (e) {
+            if (e.target.className.indexOf("side-link") != -1) {
+                componentRef.invokeMethodAsync("HideSidebar");
+            }
+        }
+    }
+}
