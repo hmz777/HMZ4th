eb29d5e0de19dbd43b8200ecdb2ec8c78d710c5a
HMZ <hamzi1995@gmail.com>
2022-01-25 18:19:05 +0200

    Animate the sidebar on open

--- src/BlogApp/wwwroot/js/modules/sidebarModule.js
+++ src/BlogApp/wwwroot/js/modules/sidebarModule.js
@@ -10,3 +10,31 @@ export function InitHandler(componentRef) {
         }
     }
 }
+
+export function InitAnimation() {
+    anime({
+        targets: '.sidebar .links .side-link',
+        translateX: [-10, 0],
+        opacity: [0, 1],
+        easing: "easeOutQuint",
+        duration: GlobalOptions.HeaderAnimationDuration,
+        delay: anime.stagger(100, { start: 100 }),
+        autoPlay: true,
+        loop: false
+    });
+
+    anime({
+        targets: '.sidebar .sidebar-social li',
+        translateY: [10, 0],
+        opacity: [0, 1],
+        easing: "easeOutQuint",
+        duration: GlobalOptions.HeaderAnimationDuration,
+        delay: anime.stagger(100, { start: 100 }),
+        autoPlay: true,
+        loop: false
+    });
+}
+
+export function Dispose() {
+
+}
