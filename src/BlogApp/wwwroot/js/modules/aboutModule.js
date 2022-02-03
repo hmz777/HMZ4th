f1968d1ab6f10384a74f31d6d0a5261546e0a9d5
HMZ <hamzi1995@gmail.com>
2022-02-03 15:50:55 +0200

    Animate the about page

--- src/BlogApp/wwwroot/js/modules/aboutModule.js
+++ src/BlogApp/wwwroot/js/modules/aboutModule.js
@@ -13,6 +13,38 @@ export function InitAnimation() {
         autoPlay: true,
         loop: false
     });
+
+    anime({
+        targets: '#about-block h2',
+        opacity: [0, 1],
+        translateY: [-20, 0],
+        easing: "easeOutQuint",
+        duration: GlobalOptions.HeaderAnimationDuration,
+        delay: anime.stagger(100, { start: 100 }),
+        autoPlay: true,
+        loop: false
+    });
+
+    anime({
+        targets: '#about-block li',
+        opacity: [0, 1],
+        translateX: [-20, 0],
+        easing: "easeOutQuint",
+        duration: GlobalOptions.HeaderAnimationDuration,
+        delay: anime.stagger(100, { start: 100 }),
+        autoPlay: true,
+        loop: false
+    });
+
+    anime({
+        targets: '#about-block .app-meta',
+        opacity: [0, 1],
+        translateY: [20, 0],
+        easing: "easeOutQuint",
+        duration: GlobalOptions.HeaderAnimationDuration,
+        autoPlay: true,
+        loop: false
+    });
 }
 
 export function Dispose() {
