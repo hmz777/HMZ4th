a88dc4a116a26dccf17ac2225bf88b8ebd95b306
HMZ <hamzi1995@gmail.com>
2022-02-04 21:22:00 +0200

    Add tag functionality

--- src/BlogApp/Pages/Tags.razor.cs
+++ src/BlogApp/Pages/Tags.razor.cs
@@ -1,14 +1,37 @@
-﻿using BlogApp.Shared;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using BlogApp.Models;
+using BlogApp.Services;
+using BlogApp.Shared;
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
 
 namespace BlogApp.Pages
 {
     public partial class Tags : TransitionPageBase<Tags>
     {
+        [Inject] IBlogPostProcessorService BlogPostProcessorService { get; set; }
+
+        [Parameter] public string Tag { get; set; }
+
+        List<YamlMetadata> Metadata;
+        List<string> TagsData;
+
+        bool DataIsValid => Metadata != null && TagsData != null;
+
+        protected override async Task OnInitializedAsync()
+        {
+            await base.OnInitializedAsync();
+
+            Metadata = await BlogPostProcessorService.ProcessPostsMetadataAsync();
+            TagsData = await BlogPostProcessorService.ProcessTagsAsync();
+        }
+
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            await base.OnAfterRenderAsync(firstRender);
+
+            if (!string.IsNullOrWhiteSpace(Tag))
+                await PageModule.InvokeVoidAsync("ScrollElementIntoView", Tag);
+        }
 
     }
 }
---
+++ src/BlogApp/wwwroot/js/modules/tagsModule.js
@@ -0,0 +1,26 @@
+﻿//#region Vars
+
+//#endregion
+
+//#region API
+
+export function InitAnimation() {
+    anime({
+        targets: '.article-header h1',
+        translateY: [500, 0],
+        easing: "easeOutQuint",
+        duration: GlobalOptions.HeaderAnimationDuration,
+        autoPlay: true,
+        loop: false
+    });
+}
+
+export function ScrollElementIntoView(id) {
+    document.getElementById(id).scrollIntoView();
+}
+
+export function Dispose() {
+
+}
+
+//#endregion
